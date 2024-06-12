2569638df21147febba3bd0d16e3c7cec73f078e
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-06-12 11:30:35 -0300

    Divisão em 4 retangulos verticais

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import pygame  
+import pygame
 
 LARGURA, ALTURA = 900, 500
 WIN = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Mini-GuitarHero")
 FPS = 60
+
+BRANCO = (255, 255, 255)
+
+def desenhar_linhas_divisao():
+    um_quarto_largura = LARGURA // 4
+    for i in range(1, 4):  # De 1 a 3 para desenhar três linhas
+        x = i * um_quarto_largura
+        pygame.draw.line(WIN, BRANCO, (x, 0), (x, ALTURA), 2)  # 2 é a largura da linha
+
+def desenhar():
+    WIN.fill((0, 0, 0))  # Preenchendo a tela
+    desenhar_linhas_divisao()
+    pygame.display.update()   # Atualizando a tela
+
+
 
 def main():
     clock = pygame.time.Clock()
@@ -13,10 +28,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        desenhar()
 
     pygame.quit()
 
-
-
-if __name__ == "__main__":  #FAZ COM QUE SÓ ABRA O PROGRAMA ATRAVÉS DO ARQUIVO MAIN
+if __name__ == "__main__":
     main()
