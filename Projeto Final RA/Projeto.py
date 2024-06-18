afde28c6a14384d02c3fdee49f2ba6ed2aceb41c
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-06-18 15:26:12 -0300

    Mini menu funciona!!!

--- Projeto Final RA/Projeto.py
+++ Projeto Final RA/Projeto.py
@@ -1,5 +1,5 @@
-import  pygame
-import os  # Sistema de operação do computador para ajudar a puxar imagens
+import pygame
+import os
 import random
 
 LARGURA, ALTURA = 900, 500
@@ -20,16 +20,15 @@ FUNDO = pygame.transform.scale(FUNDO, (LARGURA, ALTURA))
 NOTA_MUSICAL = pygame.image.load(os.path.join('Imagens', 'bola.png'))
 NOTA_MUSICAL_AJUSTADA = pygame.transform.scale(NOTA_MUSICAL, (70, 70))
 
+# Carregar imagens dos botões
+play_img = pygame.image.load(os.path.join('Imagens', 'botao_play.jpg')).convert_alpha()
+exit_img = pygame.image.load(os.path.join('Imagens', 'botao_saida.jpg')).convert_alpha()
+
 BRANCO = (255, 255, 255)
 BLACK = (0, 0, 0)
 VELOCIDADE_NOTA = 5
-DURACAO_CRIACAO_NOTAS = 100000  # 100 segundos em milissegundos
+DURACAO_CRIACAO_NOTAS = 168000  # 168 segundos em milissegundos
 INTERVALO_CRIACAO_NOTAS = 500  # 0,5 segundos em milissegundos
-
-#====== estrutura para tocar música ======#
-pygame.mixer.music.load('music2.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()     
 
 class NotaMusical:
     def __init__(self, x, y):
@@ -43,20 +42,45 @@ class NotaMusical:
     def mover(self):
         self.y += VELOCIDADE_NOTA
 
+class Button:
+    def __init__(self, x, y, image, scale):
+        largura = image.get_width()
+        altura = image.get_height()
+        self.image = pygame.transform.scale(image, (int(largura * scale), int(altura * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+play_botao = Button(90, 200, play_img, 0.2)
+exit_botao = Button(500, 200, exit_img, 0.5)
+
 def desenhar_linhas_divisao():
     um_quarto_largura = LARGURA_GUITARRA // 4
-    linha_horizontal_y = ALTURA // 2 + 100  # Linha um pouco abaixo da metade da tela
+    linha_horizontal_y = ALTURA // 2 + 100
 
-    # Calcula o deslocamento para centralizar a guitarra
     deslocamento_x = (LARGURA - LARGURA_GUITARRA) // 2
 
-    # Linhas verticais
-    for i in range(5):  # De 0 a 4 para desenhar quatro linhas dentro da guitarra e uma em cada borda
+    for i in range(5):
         x = deslocamento_x + i * um_quarto_largura
-        pygame.draw.line(WIN, BRANCO, (x, 0), (x, ALTURA), 2)  # 2 é a largura da linha
+        pygame.draw.line(WIN, BRANCO, (x, 0), (x, ALTURA), 2)
 
-    # Linha horizontal
-    pygame.draw.line(WIN, BRANCO, (deslocamento_x, linha_horizontal_y), (deslocamento_x + LARGURA_GUITARRA, linha_horizontal_y), 2)  # 2 é a largura da linha
+    pygame.draw.line(WIN, BRANCO, (deslocamento_x, linha_horizontal_y), (deslocamento_x + LARGURA_GUITARRA, linha_horizontal_y), 2)
 
 def desenhar_texto(texto, x, y, tamanho):
     fonte = pygame.font.SysFont('Arial', tamanho)
@@ -64,31 +88,29 @@ def desenhar_texto(texto, x, y, tamanho):
     WIN.blit(superficie_texto, (x, y))
 
 def desenhar(notas, contador, erros):
-    WIN.blit(FUNDO, (0, 0))  # Desenha o fundo primeiro
+    WIN.blit(FUNDO, (0, 0))
     desenhar_linhas_divisao()
     for nota in notas:
         nota.desenhar(WIN)
-    desenhar_texto(f"Contador: {contador}", 50, 100, 25)  # Desenhar o texto após desenhar as notas e linhas
-    desenhar_texto(f"Erros: {erros}", 50, 150, 25) 
-    pygame.display.update()   # Atualizando a tela
+    desenhar_texto(f"Contador: {contador}", 50, 100, 25)
+    desenhar_texto(f"Erros: {erros}", 50, 150, 25)
+    pygame.display.update()
 
 def verificar_colisao(notas, contador, quadrante):
-    # Definir o retângulo do quadrante
     um_quarto_largura = LARGURA_GUITARRA // 4
     deslocamento_x = (LARGURA - LARGURA_GUITARRA) // 2
     quadrante_x_min = deslocamento_x + quadrante * um_quarto_largura
     quadrante_x_max = quadrante_x_min + um_quarto_largura
     quadrante_y_min = ALTURA // 2 + 100
-    quadrante_y_max = quadrante_y_min + 150  # Altura da imagem da nota musical
-    erro = 1  # Assume que é um erro por padrão
+    quadrante_y_max = quadrante_y_min + 150
+    erro = 1
 
-    # Verificar se alguma nota está dentro do quadrante
     for nota in notas:
         if (quadrante_x_min <= nota.x <= quadrante_x_max and
             quadrante_y_min <= nota.y + NOTA_MUSICAL_AJUSTADA.get_height() // 2 <= quadrante_y_max):
             contador += 1
             notas.remove(nota)
-            erro = 0  # Não é um erro se a nota for acertada
+            erro = 0
             break
     return contador, erro
 
@@ -97,25 +119,20 @@ def main():
     run = True
     contador = 0
     erros = 0
-
-    # Lista para armazenar as notas musicais
     notas = []
-
     inicio_tempo = pygame.time.get_ticks()
     ultimo_tempo_criacao = inicio_tempo
-
-    # Calcula o deslocamento para centralizar a guitarra
     deslocamento_x = (LARGURA - LARGURA_GUITARRA) // 2
+    musica_tocando = False
 
     while run:
         clock.tick(FPS)
-        tempo_atual = pygame.time.get_ticks()
-        tempo_decorrido = tempo_atual - inicio_tempo
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            elif event.type == pygame.KEYDOWN:
+
+            if musica_tocando and event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     contador, erro = verificar_colisao(notas, contador, 0)
                     erros += erro
@@ -131,19 +148,32 @@ def main():
                 else:
                     erros += 1
 
-        if tempo_decorrido < DURACAO_CRIACAO_NOTAS and tempo_atual - ultimo_tempo_criacao >= INTERVALO_CRIACAO_NOTAS:
-            # Criar nova nota musical a cada 2 segundos durante os primeiros 100 segundos
-            coluna = random.randint(0, 3)
-            x = deslocamento_x + coluna * (LARGURA_GUITARRA // 4) + (LARGURA_GUITARRA // 8) - NOTA_MUSICAL_AJUSTADA.get_width() // 2
-            y = 0
-            notas.append(NotaMusical(x, y))
-            ultimo_tempo_criacao = tempo_atual
+        WIN.fill(BLACK)
+        if play_botao.draw(WIN) and not musica_tocando:
+            pygame.mixer.music.load(os.path.join('music2.mp3'))
+            pygame.mixer.music.play()
+            musica_tocando = True
 
-        for nota in notas:
-            nota.mover()
+        if exit_botao.draw(WIN):
+            run = False
 
-        
-        desenhar(notas, contador, erros)  # Chamando desenhar no final para garantir que o texto não seja sobrescrito
+        if musica_tocando:
+            tempo_atual = pygame.time.get_ticks()
+            tempo_decorrido = tempo_atual - inicio_tempo
+
+            if tempo_decorrido < DURACAO_CRIACAO_NOTAS and tempo_atual - ultimo_tempo_criacao >= INTERVALO_CRIACAO_NOTAS:
+                coluna = random.randint(0, 3)
+                x = deslocamento_x + coluna * (LARGURA_GUITARRA // 4) + (LARGURA_GUITARRA // 8) - NOTA_MUSICAL_AJUSTADA.get_width() // 2
+                y = 0
+                notas.append(NotaMusical(x, y))
+                ultimo_tempo_criacao = tempo_atual
+
+            for nota in notas:
+                nota.mover()
+
+            desenhar(notas, contador, erros)
+        else:
+            pygame.display.update()
 
     pygame.quit()
 
